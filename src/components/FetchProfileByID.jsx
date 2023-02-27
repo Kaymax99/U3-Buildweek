b5892d4a278216821c2d7a5db973850e424a607e
kla6630 <claudiopinna@live.com>
2023-02-27 13:32:46 +0100

    Create FetchProfileByID.jsx
    Create the FetchProfileByID file with Francesco

---
+++ src/components/FetchProfileByID.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const FetchProfileByID = async (idProfile) => {
+  try {
+    const BaseUrl = `https://striveschool-api.herokuapp.com/api/profile/${idProfile}`;
+
+    const response = await fetch(BaseUrl, {
+      headers: {
+        Authentication:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNmMzMmYxOTNlNjAwMTM4MDdmNTQiLCJpYXQiOjE2Nzc0ODcxNTQsImV4cCI6MTY3ODY5Njc1NH0.2ECzMWzAgGsIanmD5-fjCC-hmXzrHd0Xgq_o4L9_MQ8",
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log("tutto ok", data);
+    } else {
+      console.log("errore status", response.status);
+    }
+  } catch (error) {
+    console.log("errore nella catch", error);
+  }
+};
+
+export default FetchProfileByID;
