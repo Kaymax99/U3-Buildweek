3a82fe1a722f691813ef19c66848873205bc275d
Antonio D'Amico <andam@hotmail.it>
2023-02-27 17:42:49 +0100

    people section updated

--- src/components/PeopleSection.jsx
+++ src/components/PeopleSection.jsx
@@ -1,21 +1,29 @@
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
+import { BsFillPersonPlusFill } from "react-icons/bs";
 
 const PeopleSection = () => {
   return (
-    <Col xs={12} md={4}>
-      <h2>Persone che potresti conoscere</h2>
-      <div>
+    <Col xs={12} md={3}>
+      <h4>Persone che potresti conoscere</h4>
+      <div className="d-flex flex-column m-2 mt-3">
         <Row>
           <Col xs={2}>
             <img
-              src="https://via.placeholder.com/100"
+              src="https://via.placeholder.com/50"
               alt="people_img"
               className="people_img"
             />
           </Col>
-          <Col xs={10}></Col>
+          <Col xs={8} className="mx-3">
+            <h5>$Name </h5>
+            <p>$Title</p>
+            <Button variant="outline-secondary rounded-pill px-3">
+              {<BsFillPersonPlusFill className="mb-1 me-2" />}
+              Collegati
+            </Button>
+          </Col>
         </Row>
-        <hr />
+        <hr className="align-self-center" style={{ width: "90%" }} />
       </div>
     </Col>
   );
