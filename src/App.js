c865883e98314e3a145c76e57ab17ad1a3f51581
Simone Basile <simonebasile199@gmail.com>
2023-02-27 11:37:31 +0100

    Initial router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { CustomNavbar } from "./components/CustomNavbar";
+import { Home } from "./components/Home";
 
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <CustomNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/me" />
+        <Route path="/:profileID" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
