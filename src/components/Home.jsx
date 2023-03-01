e95154db8b81783dd54a0d6e273a3249329161bc
Antonio D'Amico <andam@hotmail.it>
2023-03-01 16:19:05 +0100

    add, edit, delete post fetch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import { addPost, fetchPosts } from "./Fetches/FetchPosts";
+import {
+  addPost,
+  fetchPosts,
+  fetchPostById,
+  deletePost,
+} from "./Fetches/FetchPosts";
 
 export const Home = () => {
   const [posts, setPosts] = useState([]);
   const [postContent, setPostContent] = useState({});
+  const [singlePost, setSinglePost] = useState(null);
   const numeroPost = 10;
   const retrievePosts = async () => {
     const data = await fetchPosts();
@@ -12,9 +18,19 @@ export const Home = () => {
     console.log(indexRandom);
     setPosts(data.slice(indexRandom, indexRandom + numeroPost));
   };
+  const retrieveSinglePost = async () => {
+    const data = await fetchPostById("63ff6067f443aa00132286e5");
+    setSinglePost(data);
+  };
+
+  const deleteSinglePost = async () => {
+    const data = await deletePost("63ff6067f443aa00132286e5");
+    setSinglePost(data);
+  };
 
   useEffect(() => {
     retrievePosts();
+    retrieveSinglePost();
   }, []);
 
   return (
@@ -46,7 +62,16 @@ export const Home = () => {
             >
               INVIA
             </button>
+            <button
+              onClick={(e) => {
+                deleteSinglePost(singlePost?.text);
+                e.preventDefault();
+              }}
+            >
+              CANCELLA
+            </button>
           </form>
+          <p>{singlePost?.text}</p>
         </div>
       </Container>
     </>
