a4257a4d66a29233cd7f1bb5167579c33a48fa59
SaraDiLuca92 <dl.sara@outlook.it>
2023-03-08 18:53:53 +0100

    css JobPage e SearchBar

--- src/components/JobsPage.jsx
+++ src/components/JobsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Container, Spinner } from "react-bootstrap";
+import { Row, Col, Container, Spinner, Card } from "react-bootstrap";
 import SearchBar from "./SearchBar";
 
 const JobFetch = () => {
@@ -10,7 +10,9 @@ const JobFetch = () => {
   useEffect(() => {
     setIsLoading(true);
     const fetchJobs = async () => {
-      const response = await fetch(`https://strive-benchmark.herokuapp.com/api/jobs?search=${jobName}`);
+      const response = await fetch(
+        `https://strive-benchmark.herokuapp.com/api/jobs?search=${jobName}`
+      );
       if (response.ok) {
         const jobsArray = await response.json();
         setJobs(jobsArray.data);
@@ -34,20 +36,23 @@ const JobFetch = () => {
   return (
     <>
       <div className="container newnew mt-3">
-        <div className="my-3">
+        <div className="divsearch my-3">
           <SearchBar placeholder="Search for jobs" onSearch={handleSearch} />
         </div>
 
-        <div className="my-5">
+        <div className="jobcards my-5">
           {jobs.length > 0 && !isLoading ? (
             jobs.map((job) => (
               <Row className="row_big" key={"job" + job._id}>
                 <Col sm={12} md={8} className="Col_01">
-                  <div>
-                    <h4>Job</h4>
-                    <h2>{job.title}</h2>
-                    <h5>Azienda: {job.company_name}</h5>
-                  </div>
+                  <Card style={{ width: "18rem", height: "10rem" }}>
+                    <Card.Body>
+                      <Card.Title>{job.title}</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">
+                        Azienda: {job.company_name}
+                      </Card.Subtitle>
+                    </Card.Body>
+                  </Card>
                 </Col>
               </Row>
             ))
