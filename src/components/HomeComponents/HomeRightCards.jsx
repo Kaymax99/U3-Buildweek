d9cd9ec5346b5dee862d229499cfc4503ddf147f
Antonio D'Amico <andam@hotmail.it>
2023-03-08 10:23:18 +0100

    Update HomeRightCards.jsx

--- src/components/HomeComponents/HomeRightCards.jsx
+++ src/components/HomeComponents/HomeRightCards.jsx
@@ -1,42 +1,59 @@
-import { Card, Spinner } from "react-bootstrap";
+import { Card, Col, Container, Row, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logoLinkedin from "../../assets/imgs/Linkedin-Logo-700x394.png";
+import { InfoSquareFill } from "react-bootstrap-icons";
+import { ChevronCompactDown, ChevronCompactUp } from "react-bootstrap-icons";
+import { useState } from "react";
+import SingleNews from "./SingleNews";
 
 export const HomeRightCards = ({ titles }) => {
+  const [otherNews, setOtherNews] = useState(false);
+  const [newsParam, setNewsParam] = useState(5);
+
   return (
     <>
-      <Card>
-        <Card.Body>
-          <Card.Title className="bold">LinkedIn Notizie</Card.Title>
-
-          <div>
-            <ul className="listaNews">
-              {titles?.length > 0 ? (
-                titles.slice(0, 5).map((post, i) => (
-                  <li key={i}>
-                    By:
-                    <a href={`/` + post.user._id}>
-                      <p className="bold">{post.user.name}</p>
-                    </a>
-                    Contenuto:
-                    <p className="bold"> {post.text.slice(0, 20)}...</p>
-                  </li>
-                ))
-              ) : (
+      <Container className="news-container rounded pb-2">
+        <Row className="justify-content-between">
+          <Col xs={10} className="px-2 pt-2">
+            <p className="mb-2 bold">LinkedIn Notizie</p>
+          </Col>
+          <Col xs={2} className="pt-2 icona-news">
+            <InfoSquareFill />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="ms-3">
+            <ul className="lista">
+              {titles.length === 0 ? (
                 <div className="text-center my-5">
                   <Spinner variant="primary" />
                 </div>
+              ) : (
+                titles
+                  .slice(0, newsParam)
+                  .map((post, i) => <SingleNews key={i} post={post} />)
               )}
             </ul>
-          </div>
-
-          {titles?.length > 0 ? (
-            <Card.Link href="#">Visualizza altro</Card.Link>
-          ) : (
-            ""
+          </Col>
+        </Row>
+        <Row>
+          {titles?.length > 0 && (
+            <div className="mt-2">
+              <button
+                onClick={() => {
+                  setOtherNews(!otherNews);
+                  !otherNews ? setNewsParam(10) : setNewsParam(5);
+                }}
+                href="#/"
+                className="altro"
+              >
+                {!otherNews ? "Visualizza altro " : "Mostra meno "}
+                {!otherNews ? <ChevronCompactDown /> : <ChevronCompactUp />}
+              </button>
+            </div>
           )}
-        </Card.Body>
-      </Card>
+        </Row>
+      </Container>
       <Card className="imgbcard rounded">
         <Card.Img
           variant="top"
