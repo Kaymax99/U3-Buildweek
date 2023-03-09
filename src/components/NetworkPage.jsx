b1ef6eac43d15abb937a9c9d5463777ec9d1dd91
Antonio D'Amico <andam@hotmail.it>
2023-03-09 11:36:00 +0100

    Update NetworkPage.jsx

--- src/components/NetworkPage.jsx
+++ src/components/NetworkPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useState, useEffect } from "react";
 import { Card, Button, Row, Col, Form } from "react-bootstrap";
 import { Link } from "react-bootstrap-icons";
